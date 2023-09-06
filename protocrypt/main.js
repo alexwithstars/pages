2d1952943d5dc3c3375f13075396be16fdb78297
alexwithstars <rayo080378@gmail.com>
2023-09-05 21:57:58 -0600

    feat (protocrypt): new page

---
+++ protocrypt/main.js
@@ -0,0 +1,127 @@
+"use strict";
+import {getDictionary} from "./encrypt.js"
+const qs = sel => document.querySelector(sel)
+const qsa = sel => document.querySelectorAll(sel)
+const gti = sel => document.getElementById(sel)
+const gtg = sel => document.getElementsByTagName(sel)
+
+const chr = n=>String.fromCodePoint(n)
+const ord = n=>n.codePointAt(0)
+
+const user = gti("user")
+const password = gti("key")
+const show = gti("show")
+const finish = gti("finish")
+const mode = gti("mode")
+const text = gti("text")
+const title = gti("title")
+
+function delay(time){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve()
+		},time)
+	})
+}
+async function tagInfo(tx,error){
+	let element = document.createElement("div")
+	element.textContent=tx
+	element.classList.add(error?"error":"done")
+	document.body.appendChild(element)
+	await delay(50)
+	element.classList.add("showin")
+	setTimeout(async ()=>{
+		element.classList.remove("showin")
+		await delay(500)
+		document.body.removeChild(element)
+	},2000)
+}
+
+function encryptUser(username,pass){
+	let arr = getDictionary(pass)
+	let usercrypt=""
+	let passcrypt=""
+	for(let i of username){
+		usercrypt+=chr(arr[ord(i)-32])
+	}
+	for(let i of pass){
+		passcrypt+=chr(arr[ord(i)-32])
+	}
+	return {user:usercrypt,pass:passcrypt}
+}
+
+function charVerify(e){
+	let target=e.target==password?e.target.parentElement:e.target
+	for(let i of e.target.value){
+		if(ord(i)>127 || ord(i)<32){
+			target.classList.add("invalid")
+			tagInfo(`caracteres no validos (${i})`,true)
+			return
+		}
+	}
+	target.classList.remove("invalid")
+}
+user.addEventListener("keyup",charVerify)
+password.addEventListener("keyup",charVerify)
+
+show.addEventListener("change",(e)=>{
+	document.startViewTransition(()=>{
+		if(show.checked){
+			password.type="text"
+			return
+		}
+		password.type = "password"
+	})
+})
+let lr=false
+mode.addEventListener("click",()=>{
+	lr=!lr
+	document.startViewTransition(()=>{
+		title.textContent=lr?"Register":"Login"
+		text.textContent=lr?"Ya tienes una cuenta?":"Aun no estas registrado?"
+		finish.innerHTML=`<span>${title.textContent}</span>`
+	})
+})
+
+function bdAction(e){
+	if(lr){
+		if(user.value.length<5){
+			tagInfo("Usuario demasiado corto (al menos 5 caracteres)",true)
+			return
+		}
+		if(password.value.length<8){
+			tagInfo("Contraseña demasiado corta (al menos 8 caracteres)",true)
+			return
+		}
+	}
+	let acount = encryptUser(user.value,password.value)
+	if(lr){
+		if(localStorage.getItem(acount.user)){
+			tagInfo("Usuario existente",true)
+			return
+		}
+		localStorage.setItem(acount.user,acount.pass)
+		tagInfo("Usuario registrado",false)
+	}
+	else{
+		if(localStorage.getItem(acount.user)){
+			if(acount.pass==localStorage.getItem(acount.user)){
+				tagInfo("acceso correcto",false)
+			}
+		}
+		else{
+			tagInfo("Usuario o contraseña no validos",true)
+		}
+	}
+}
+finish.addEventListener("click",bdAction)
+user.addEventListener("keydown",(e)=>{
+	if(e.key=="Enter"){
+		password.focus()
+	}
+})
+password.addEventListener("keydown",(e)=>{
+	if(e.key=="Enter"){
+		bdAction(e)
+	}
+})
